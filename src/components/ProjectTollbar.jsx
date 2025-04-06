0fdc7a80d2772d63086825867d67bac5a7d27c1b
Mukunthan <mukunthan.1255@gamil.com>
2025-04-06 09:32:42 +0530

    refactor code

--- src/components/ProjectTollbar.jsx
+++ src/components/ProjectTollbar.jsx
@@ -3,15 +3,13 @@ import MenuItem from "./MenuItem";
 
 const ProjectTollbar = () => {
   return (
-    <div className=" bg-blue-900 p-6">
-      <ul>
-        <MenuItem text="Creat Project" />
-        <MenuItem text="Edit User" />
-        <MenuItem text="Format Projects" />
-        <MenuItem text="Settings" />
-        <MenuItem text="Logout" />
-      </ul>
-    </div>
+    <ul className="p-6">
+      <MenuItem text="Creat Project" />
+      <MenuItem text="Edit User" />
+      <MenuItem text="Format Projects" />
+      <MenuItem text="Settings" />
+      <MenuItem text="Logout" />
+    </ul>
   );
 };
 
