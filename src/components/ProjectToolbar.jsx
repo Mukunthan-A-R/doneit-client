6e9281e05e5bd254c046f48fa687ce940f738213
Mukunthan <mukunthan.1255@gamil.com>
2025-04-15 11:27:14 +0530

    Implementing Logout

--- src/components/ProjectToolbar.jsx
+++ src/components/ProjectToolbar.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import MenuItem from "./modals/MenuItem";
 import CreateProject from "./modals/CreateProject";
 import FormatProjects from "./modals/FormatProjects";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { userData } from "../data/atom"; // Update path if needed
 
-const ProjectToolbar = ({user_id, handleTrigger}) => {
+const ProjectToolbar = ({ user_id, handleTrigger }) => {
   const [showModal, setShowModal] = useState(false);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [user, setUser] = useRecoilState(userData);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("x-auth-token");
+    setUser({});
+    navigate("/login");
+  };
 
   return (
     <>
@@ -17,12 +28,21 @@ const ProjectToolbar = ({user_id, handleTrigger}) => {
         <button onClick={() => setIsPopupOpen(true)}>
           <MenuItem text="Format Projects" />
         </button>
+
         <MenuItem text="Settings" />
-        <MenuItem text="Logout" />
+
+        <button onClick={() => handleLogout()}>
+          <MenuItem text="Logout" />
+        </button>
       </ul>
 
       {/* Modals */}
-      <CreateProject handleTrigger={handleTrigger} user_id={user_id} showModal={showModal} setShowModal={setShowModal} />
+      <CreateProject
+        handleTrigger={handleTrigger}
+        user_id={user_id}
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
       {isPopupOpen && <FormatProjects onCancel={setIsPopupOpen} />}
     </>
   );
