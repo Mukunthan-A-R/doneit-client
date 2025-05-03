fe375cbd8acf94d00997303fa229728448031cd1
Mukunthan A R <mukunthan.1255@gmail.com>
2025-05-03 08:01:02 +0530

    remove commes fix

--- src/components/AddUserRoles.jsx
+++ src/components/AddUserRoles.jsx
@@ -124,8 +124,8 @@ const AddUserRoles = () => {
     }
   };
 
-  console.log("userRole");
-  console.log(userRole);
+  // console.log("userRole");
+  // console.log(userRole);
 
   return (
     <div className="max-w-4xl mx-auto p-6">
