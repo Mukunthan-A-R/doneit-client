7b1c9ee838a850dc9b9c17ed1a092b63d8881100
Mukunthan A R <mukunthan.1255@gmail.com>
2025-06-22 11:59:18 +0530

    fixing userDashboard

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -7,6 +7,7 @@ import { userData } from "../data/atom";
 import { fetchProjects } from "../services/ProjectServices";
 import { fetchUserById } from "../services/UserData";
 import { fetchTasksByUserId } from "../services/miscService";
+import SettingsPage from "../components/SettingsPage.jsx";
 
 const UserDashboard = () => {
   const { user } = useRecoilValue(userData);
@@ -14,14 +15,7 @@ const UserDashboard = () => {
   const [totalProjects, setTotalProjects] = useState([]);
   const [completedProjects, setCompletedProjects] = useState(0);
   const [overdueProjects, setOverdueProjects] = useState(0);
-  const [showEditModal, setShowEditModal] = useState(false);
   const [todaysTasks, setTodaysTasks] = useState([]);
-  const [userDetails, setUserDetails] = useState({
-    name: "",
-    email: "",
-    company: "",
-    role: "",
-  });
 
   useEffect(() => {
     const loadProjects = async () => {
@@ -65,21 +59,6 @@ const UserDashboard = () => {
   }, [user.user_id]);
 
   useEffect(() => {
-    const loadUserDetails = async () => {
-      try {
-        const response = await fetchUserById(user.user_id);
-        if (response.success && response.status === 200) {
-          setUserDetails(response.data);
-        }
-      } catch (error) {
-        console.error("Error fetching user:", error);
-      }
-    };
-
-    loadUserDetails();
-  }, [user.user_id]);
-
-  useEffect(() => {
     const getTasks = async () => {
       try {
         const data = await fetchTasksByUserId(user.user_id);
@@ -94,44 +73,10 @@ const UserDashboard = () => {
 
   return (
     <div className="relative flex overflow-hidden text-gray-800 font-sans">
-      {showEditModal && (
-        <EditUserModal
-          handleSetUserDetails={(data) => {
-            setUserDetails(data);
-          }}
-          onClose={() => setShowEditModal(false)}
-        />
-      )}
-
       {/* Main Content */}
       <div className="flex-1 flex flex-col">
         {/* User Info Section */}
-        <section className="bg-white p-4 mt-6 rounded-lg shadow flex items-center justify-between border-l-4 border-blue-700">
-          <div className="flex items-center space-x-4">
-            <img
-              className="h-16 w-16 rounded-full border-2 border-blue-700"
-              src="https://i.pravatar.cc/100?img=2"
-              alt="User"
-            />
-            <div>
-              <h2 className="text-xl font-semibold text-blue-900">
-                {userDetails.name}
-              </h2>
-              <p className="text-sm text-gray-600">
-                {userDetails.role} @ ( {userDetails.company} )
-              </p>
-              <p className="text-sm text-gray-500">{userDetails.email}</p>
-            </div>
-          </div>
-          <button
-            className="text-blue-700 hover:underline"
-            onClick={() => {
-              setShowEditModal(true);
-            }}
-          >
-            Edit Profile
-          </button>
-        </section>
+        <SettingsPage></SettingsPage>
 
         {/* Dashboard Content */}
         <main className="flex-1 overflow-y-auto pt-4">
@@ -200,8 +145,8 @@ const UserDashboard = () => {
                           project.priority === "high"
                             ? "bg-red-500"
                             : project.priority === "medium"
-                              ? "bg-yellow-500"
-                              : "bg-green-500"
+                            ? "bg-yellow-500"
+                            : "bg-green-500"
                         }`}
                       >
                         {project.priority}
