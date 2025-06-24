c5f2639174bff97cb032edd507bd2705679448ba
Sachin / <sreesachin11226@gmail.com>
2025-06-24 20:46:52 +0530

    Fix typo in header and handle errors in AddUserRoles component

--- src/pages/AddProjectUser.jsx
+++ src/pages/AddProjectUser.jsx
@@ -11,7 +11,7 @@ const AddProjectUser = () => {
       <ProjectTitleCard project_id={project_id}></ProjectTitleCard>
 
       <header className="bg-blue-950 text-white py-4 px-6 shadow rounded-lg flex flex-col md:flex-row items-start md:items-center justify-between mb-6 gap-4 md:gap-0">
-        <h1 className="text-2xl font-bold"> Add Collobrator</h1>
+        <h1 className="text-2xl font-bold">Add Collaborator</h1>
       </header>
 
       <AddUserRoles />
